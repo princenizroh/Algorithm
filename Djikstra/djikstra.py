cc87f3bd7dedc86955917fcb778943bbcb533704
princenizroh <princenizroh7@gmail.com>
2023-12-05 23:44:33 +0800

    merge azhar

--- Djikstra/djikstra.py
+++ Djikstra/djikstra.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 Graph = Dict[str, Dict[str, int]]
 # Create a graph class
-class Djikstra(object):
+class Dijkstra(object):
     # Constructor for the graph
     def __init__(self, nodes: List[str], graph: Graph, source: str) -> None:
         self.nodes = nodes
@@ -18,134 +18,72 @@ class Djikstra(object):
         self.previous_nodes = {source: source}
         self.history = []
     
-    # Construct the graph  
-    def construct_graph(self, nodes, init_graph)->dict:
-        graph = {}
-        for node in nodes:
-            graph[node] = {}
-        graph.update(init_graph)
-
-        # Check if the graph is symmetric
-        for node, edges in graph.items():
-            for adjacent_node, value in edges.items():
-                # If the graph is not symmetric, add the reverse edge
-                if graph[adjacent_node].get(node, False) == False:
-                    graph[adjacent_node][node] = value
-        return graph
-
-    # Get the nodes
-    def get_nodes(self)->list:
-        return self.nodes
-
-    # Get the edges
-    def get_edges(self, node)->list:
-        return list(self.graph[node].keys())
-
-    # Get the value of the edges
-    def get_value(self, node1, node2)->int:
-        return self.graph[node1][node2]
-
-    # Dijkstra's algorithm
-    def dijkstra(self, start_node)->None:
-        # Initialize the algorithm
-        unvisited_nodes = sorted(self.get_nodes())
-        shortest_distance = {node:float("inf") for node in unvisited_nodes}
-        previous_nodes = {}
-
-        # Initialize the distance of the start node to 0
-        for node in unvisited_nodes:
-            shortest_distance[node] = float("inf")
-            print(shortest_distance[node])
-        shortest_distance[start_node] = 0
-
-        iteration = 1
-
-        # Start the algorithm
-        while unvisited_nodes:
-            current_min_node = None
-
-            # Find the node with the shortest distance
-            for node in unvisited_nodes:
+    def start(self) -> None:
+        while self.unvisited_nodes:
+            current_min_node = self.unvisited_nodes[0]
+            for node in self.unvisited_nodes:
                 if (
-                    current_min_node is None
-                    or shortest_distance[node] < shortest_distance[current_min_node]
+                    self.shortest_distance[node]
+                    < self.shortest_distance[current_min_node]
                 ):
-                    print("shortest distance",shortest_distance[node])
                     current_min_node = node
 
-            # Find the distance from the current node to its neighbors
-            neighbors = self.get_edges(current_min_node)
+            neighbours = list(self.graph[current_min_node].keys())
+            for neighbour in neighbours:
+                neighbour_edge = self.graph[current_min_node][neighbour]
+                distance = self.shortest_distance[current_min_node] + neighbour_edge
 
-            # Update the distance of the neighbors
-            for neighbor in neighbors:
-                distance = shortest_distance[current_min_node] + self.get_value(
-                    current_min_node, neighbor
-                )
-                if distance < shortest_distance[neighbor]:
-                    shortest_distance[neighbor] = distance
-                    previous_nodes[neighbor] = current_min_node
+                if distance < self.shortest_distance[neighbour]:
+                    self.shortest_distance[neighbour] = distance
+                    self.previous_nodes.update({neighbour: current_min_node})
 
-            unvisited_nodes.remove(current_min_node)
-
-            # Print the result of the current iteration
-            print(f"\nStep {iteration}:")
-            print(
-                f"Now we need to start checking the distance from node {current_min_node} to its adjacent nodes."
+            self.history.append(
+                [
+                    current_min_node,
+                    self.shortest_distance.copy(),
+                    self.previous_nodes.copy(),
+                ]
             )
+            self.unvisited_nodes.remove(current_min_node)
 
-            if iteration == 1:
-                print(
-                    f"We mark it with a red square in the list to represent that it has been 'visited' and that "
-                    f"we have found the shortest path to this node:"
-                )
-            else:
-                print(
-                    f"We cross it off from the list of unvisited nodes: Unvisited Nodes: {list(unvisited_nodes)}"
-                )
+    def table(self) -> None:
+        data = [[self.source, 0, "-"]]
+        for node in self.nodes:
+            if node == self.source:
+                continue
+            shortestDist = self.shortest_distance[node]
+            prevNode = self.previous_nodes[node]
+            data.append([node, shortestDist, prevNode])
+        print(tabulate(data, ["Node", "Shortest", "Previous"], tablefmt="fancy_grid"))
 
-            result_table = [
-                [node, shortest_distance[node]]
-                if node in shortest_distance and shortest_distance[node] != float("inf")
-                else [node, "∞"]
-                for node in self.nodes
-            ]
-            headers = ["Node", "Shortest Distance"]
-            print(
-                tabulate(result_table, headers=headers, tablefmt="grid", missingval="")
-            )
 
-            iteration += 1
+    def dimpram(self) -> None:
+        data = []
+        for historyItem in self.history:
+            row = [historyItem[0]]
+            for node, dist in historyItem[1].items():
+                try:
+                    row.append(f"{ dist }_{ historyItem[2][node] }")
+                except KeyError:
+                    row.append(dist)
+            data.append(row)
 
-        # Print the final result
-        self.shortest_distance = shortest_distance
-        self.previous_nodes = previous_nodes
+        print(tabulate(data, ["V", *nodes], tablefmt="fancy_grid"))
 
-    # Print the result
-    def print_result(self, target_node)->None:
-        unvisited_nodes = set(self.nodes)
-
-        start_node = nodes[0]
-
-        if target_node not in self.shortest_distance:
-            print(f"There is no path from {start_node} to {target_node}")
-            return
+    def construct_path(self, destination) -> None:
+        if destination not in self.shortest_distance:
+            return print(f"Path dari {self.source} ke {destination} tidak ditemukan")
 
         path = []
-        node = target_node
 
-        while node != start_node:
+        node = destination
+        while node != self.source:
             path.append(node)
             if node not in self.previous_nodes:
-                print(f"Error: No path found from {start_node} to {target_node}")
-                return
+                return print(f"Path dari {self.source} ke {destination} tidak ditemukan")
             node = self.previous_nodes[node]
 
-        path.append(start_node)
-
-        print(
-            f"We found the following best path with a value of {self.shortest_distance[target_node]}."
-        )
-
+        path.append(self.source)
         print(" -> ".join(reversed(path)))
 
     # Visualize the graph
@@ -176,29 +114,23 @@ class Djikstra(object):
         plt.show()
 
 
-nodes = ["A", "B", "C", "D", "E", "F", "G"]
-init_graph = {
-    "A": {"B": 5, "C": 7, "D": 12},
-    "B": {"C": 1, "E": 6},
-    "C": {"D": 1, "E": 5, "F": 4},
-    "D": {"F": 13},
-    "E": {"F": 2, "G": 7},
-    "F": {"G": 3},
-}
-# nodes = ["Tempest", "Fittoa", "Asura", "Ranoa", "Shiron", "Milis", "Dwargon"]
-# init_graph = {
-#     "Tempest": {"Fittoa": 3, "Asura": 4, "Ranoa": 6},
-#     "Fittoa": {"Milis": 1, "Shiron": 6},
-#     "Asura": {"Fittoa": 2, "Milis": 3},
-#     "Ranoa": {"Dwargon": 2},
-#     "Shiron": {"Dwargon": 1},
-#     "Milis": {"Dwargon": 4},
-# }
+if __name__ == "__main__":
+    nodes = ["A", "B", "C", "D", "E", "F", "G"]
+    init_graph = {
+        "A": {"B": 5, "C": 7, "D": 12},
+        "B": {"C": 1, "E": 6},
+        "C": {"D": 1, "E": 5, "F": 4},
+        "D": {"F": 13},
+        "E": {"F": 2, "G": 7},
+        "F": {"G": 3},
+        "G": {},
+    }
+    destination = "G"
 
-my_graph = Graph(nodes, init_graph)
-# my_graph.dijkstra("Tempest")
-# my_graph.print_result(target_node="Dwargon")
+    dijkstra = Dijkstra(nodes, init_graph, "A")
 
-my_graph.dijkstra("A")
-my_graph.print_result(target_node="G")
-# my_graph.visualize()
+    dijkstra.start()
+    dijkstra.table()
+    dijkstra.dimpram()
+    dijkstra.construct_path(destination)
+    dijkstra.visualize()
